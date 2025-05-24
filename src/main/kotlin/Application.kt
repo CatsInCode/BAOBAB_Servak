56f10e6168dbcb449508a4edb294ea7a00ccabe5
Gorokhov <roundcubik@gmail.com>
2025-05-24 21:40:40 +0300

    LogRegBase 0.1

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,11 +2,23 @@ package com.example
 
 import com.example.database.DatabaseFactory
 import com.example.plugins.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.routing.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
-fun Application.module() {
-    DatabaseFactory.init()
-    configureRouting()
+fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080  // Важно для Render
+    embeddedServer(CIO, port = port, host = "0.0.0.0") {     // CIO → Netty для production
+        install(ContentNegotiation) {
+            json()
+        }
+        DatabaseFactory.init()
+        routing {
+            configureRouting()
+        }
+    }.start(wait = true)
 }
